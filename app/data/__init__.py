ac7910b65001aec98c5f0f15a285b5eecd62b609
tmt514 <math.tmt514@gmail.com>
2017-02-18 04:16:48 -0500

    data initialization

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -1,8 +1,8 @@
 from .parsedishes import add_dishes
 from .parsevegetables import add_vegetables
-
+from .parsepokemon import add_pokemons
 
 def prepare_data(dryrun=True):
     add_dishes(dryrun)
     add_vegetables(dryrun)
-
+    add_pokemons(dryrun)
